35509ee6213031530620f3fc4da56c56b67c7d8a
Ilham Harazki <ilhamharazky@rocketmail.com>
2019-01-14 15:06:32 +0700

    Variabel
    Pembelajaran pertama

---
+++ src/basics/Variable.kt
@@ -0,0 +1,19 @@
+package basics
+
+fun main(args: Array<String>) {
+
+    /*===== var variable (mutable)=====*/
+    var namaJalan   : String
+    var nomorJalan  : String
+
+    namaJalan   = "Veteran"
+    nomorJalan  = " 50"
+
+    println("Jalan $namaJalan / $nomorJalan")
+
+    /*===== val variable (immutable)=====*/
+    val kodePos     : Int
+
+    kodePos     = 1456789
+    println("Kode Pos : $kodePos")
+}
